6ca306127254f284b49677017be691ad2fe66b07
alamin2 <alamin@co.design>
2021-10-11 15:48:17 +0600

    First two section done

--- components/banner.jsx
+++ components/banner.jsx
@@ -8,9 +8,9 @@ const Banner = () => {
           <h2>The World has chastised them from public view</h2>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+            industry. Lorem Ipsum has been the industry&apos;s standard dummy
+            text ever since the 1500s, when an unknown printer took a galley of
+            type and scrambled it to make a type specimen book.
           </p>
         </div>
         <div className="banner-blur"></div>
--- components/feature.jsx
+++ components/feature.jsx
@@ -38,9 +38,9 @@ const Feature = () => {
           <div className="line"></div>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+            industry. Lorem Ipsum has been the industry&apos;s standard dummy
+            text ever since the 1500s, when an unknown printer took a galley of
+            type and scrambled it to make a type specimen book.
           </p>
           <button>Get Started</button>
         </div>
