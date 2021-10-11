a3cbababf2e4f65b71f601a25bd494967762e051
alamin2 <alamin@co.design>
2021-10-12 00:43:41 +0600

    Team and Footer left

---
+++ components/roadmap.jsx
@@ -0,0 +1,116 @@
+const Roadmap = () => {
+  return (
+    <section className="roadmap">
+      <h2 className="heading">RoadMap</h2>
+      <p className="roadmap-text">
+        10% of all funds collected will be contributed to a charity (With
+        Onchain Proof),
+        <br /> so you will get a cool nipple that with the power of the
+        community and NFN(Non-Fungiblenomics)retains its value and you will also
+        aid to a good cause.
+      </p>
+      <div className="roadmap-row">
+        <img src="/curve1.png" alt="" />
+        <div className="roadmap-box">
+          <h2>0% The journey begins, welcome to the Nippleverse</h2>
+          <ul>
+            <li>
+              -The minting process begins with 0.08008 Eth(boobs lol) as a
+              minting price
+            </li>
+            <li>
+              -A private discord channel gets unlocked(Only for Nipple owners)
+            </li>
+            <li>-Release of 1/1 Halloween Nipple</li>
+            <li>-One airdropped nipple to the first 69 buyers</li>
+            <li>
+              -Owners get a special seat in what will appear next in the
+              multiverse.
+            </li>
+            <li>-1ETH giveaway to the first 100buyers</li>
+          </ul>
+        </div>
+        <div className="roadmap-box">
+          <h2>25%-Expanding the Nipplevers</h2>
+          <ul>
+            <li>-Rewards for active Discord members(Invite Contest)</li>
+            <li>-10% sales charity donation equal to aprox 32000$</li>
+            <li>-1ETH+1 Nipple community airdrop to random holders</li>
+            <li>-Because rare traits aren&apos;t the only beautiful thing, </li>
+            <li>
+              there will be a weekly contest where the community can vote the
+              best looking nipple, winners will be displayed in a special
+              gallery named &apos;&apos;Beauty lays in the nipple of the
+              beholder&apos;&apos;.
+            </li>
+          </ul>
+        </div>
+        <div className="roadmap-box">
+          <h2>50%- 4th Wall break</h2>
+          <ul>
+            <li>-Meme contest, the winner will receive 1ETH and 1 nipple</li>
+            <li>
+              -Retweet giveaway: A random person that retweets and follows gets
+              a nipple
+            </li>
+            <li>-3ETH community airdrop to random holders</li>
+            <li>
+              -Nipples worth 32500$ will be bought and burned to lift the floor
+              price
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="roadmap-row roadmap-row-last">
+        <img src="/curve2.png" alt="" />
+        <div className="roadmap-box">
+          <h2>75%-Vincent van Nipple</h2>
+          <ul>
+            <li>
+              -Nipple Art contest: Use nipples in the most creative ways and
+              post them on twitter with the #nippleverse hashtag, the winner
+              gets 1ETH
+            </li>
+            <li>-5ETH community airdrop to random holders</li>
+            <li>
+              -Nipples worth 50000$ will be bought and burned to lift the floor
+              price
+            </li>
+          </ul>
+        </div>
+        <div className="roadmap-box">
+          <h2>100%-The ETHernal nippleverse</h2>
+          <ul>
+            <li>
+              -10% of royalty fees will get airdropped to holders as ETH monthly
+            </li>
+            <li>-7ETH community airdrop to random holders</li>
+            <li>
+              -Nipples worth 65000$ will be bought and burned to lift the floor
+              price
+            </li>
+            <li>
+              -Special antimatter formula that can be used to enhance your
+              nipples in the future
+            </li>
+            <li>-50% of royalty fees will be used to buyback and burn nfts</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Roadmap;
+{
+  /* <div className="roadmap-box">
+          <h2></h2>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li> </li>
+            <li></li>
+          </ul>
+        </div> */
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Banner from "../components/banner";
 import Feature from "../components/feature";
 import Navbar from "../components/navbar";
+import Roadmap from "../components/roadmap";
 export default function Home() {
   return (
     <div className="body-wrapper">
@@ -13,10 +14,17 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&family=Roboto:wght@300;400;500&display=swap"
           rel="stylesheet"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <Navbar />
       <Banner />
       <Feature />
+      <Roadmap />
     </div>
   );
 }
